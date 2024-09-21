02340e2e7622b40c90568ce25d0fc5f6d1d8272b
Bhavya kumar <166162581+Bhavya-Creates@users.noreply.github.com>
2024-09-21 12:08:08 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+document.getElementById('buyNowBtn').addEventListener('click', function() {
+    alert('Thank you for your purchase!');
+});
+
+document.getElementById('newsletterForm').addEventListener('submit', function(event) {
+    event.preventDefault();
+    alert('Thank you for subscribing to our newsletter!');
+});
